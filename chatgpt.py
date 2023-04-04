e556b4e264a1bbad76fafea54e5629a42242b60b
Matteo Cancellieri <matteo.cancellieri@open.ac.uk>
2023-04-04 11:38:29 +0100

    core gpt improved version

---
+++ chatgpt.py
@@ -0,0 +1,72 @@
+import openai
+import json
+
+***REMOVED***
+#ou key for gpt 4
+***REMOVED***
+
+def generate_search_query(input_request):
+    messages = [
+        {"role": "system", "content": "Generate a search engine query for a research paper based on the question. "
+                                      "Prioritise the most important keywords and add synonyms to focus the search. "
+                                      "Ensure that the response contain only the query and no other extra text"
+                                      "The answer should be no longer than "
+                                      "80 words."},
+        {"role": "user", "content": f"{input_request}"},
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages
+    )
+    return response["choices"][0]["message"]["content"].replace("\"", "").replace(":", "")
+
+
+def generate_answer(input_request, search_results):
+    global final_answer
+    messages = [
+        {"role": "system", "content": "Generate a comprehensive answer (but no more than 160 words) "
+                                      "for a given question solely based on the provided search results in the format: "
+                                      "{url:$url, abstract:$abstract}. "
+                                      "You must only use information from the provided search results."
+                                      "Use an unbiased and journalistic tone. Combine search results together "
+                                      "into a coherent answer. "
+                                      "Do not repeat text. Cite search results using the url provided and the [N] "
+                                      "notation. Only "
+                                      "cite the most relevant result that answer the question "
+                                      "accurately. If different results refer to different entities with the same "
+                                      "name, write separate answers for each entity."},
+        {"role": "user", "content": f"{input_request}"},
+        {"role": "assistant", "content": f"{json.dumps(search_results)}"}
+    ]
+
+    print(messages)
+    finalResponse = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages
+    )
+    return finalResponse["choices"][0]["message"]["content"]
+
+def generate_course_material(input_request, search_results):
+    global final_answer
+    messages = [
+        {"role": "system", "content": "Generate a comprehensive course reading list for undergraduate students"
+                                      "for a given topic solely based on the provided search results in the format: "
+                                      "{url:$url, abstract:$abstract}. "
+                                      "You must only use information from the provided search results."
+                                      "Use an unbiased and journalistic tone. Combine search results together "
+                                      "into a coherent course material. "
+                                      "Do not repeat text. Cite search results using the url provided and the [N] "
+                                      "notation. Only "
+                                      "cite the most relevant result that answer the question "
+                                      "accurately. If different results refer to different entities with the same "
+                                      "name, write separate answers for each entity."},
+        {"role": "user", "content": f"{input_request}"},
+        {"role": "assistant", "content": f"{json.dumps(search_results)}"}
+    ]
+
+    print(messages)
+    finalResponse = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return finalResponse["choices"][0]["message"]["content"]
---
+++ core.py
@@ -0,0 +1,57 @@
+import requests
+
+import dateutil.parser
+from datetime import datetime, timezone
+import requests
+import time
+import urllib.parse
+
+api_key = "***REMOVED***"
+limit = 5
+
+
+def query_api(search_url, query, scroll=False, scrollId=None):
+    headers = {"Authorization": "Bearer " + api_key.strip()}
+
+    if not scrollId and scroll:
+        print(f"{search_url}?q={query}&limit={limit}&scroll=true")
+        response = requests.get(f"{search_url}?q={query}&exclude=fullText&limit={limit}&scroll=true", headers=headers)
+    elif scroll and scrollId:
+        print(f"{search_url}?q={query}&limit={limit}&scrollId={scrollId}")
+        response = requests.get(f"{search_url}?q={query}&exclude=fullText&limit={limit}&scrollId={scrollId}",
+                                headers=headers)
+    else:
+        print(f"{search_url}?q={query}&limit={limit}")
+        response = requests.get(f"{search_url}?q={query}&exclude=fullText&limit={limit}", headers=headers)
+    print(response.status_code)
+    if response.status_code == 429:
+        retryAfter = dateutil.parser.parse(response.headers['X-RateLimit-Retry-After'])
+        now = datetime.now(timezone.utc)
+        if retryAfter > now:
+            sleepTime = retryAfter - now
+            print(f"Sleeping for {sleepTime.seconds + 1}")
+            time.sleep(sleepTime.seconds + 1)
+        return query_api(search_url, query, True, scrollId)
+    if response.status_code > 499:
+        print(response.status_code)
+        print(response.content)
+        print("Sleeping for 5")
+        time.sleep(5)
+        return query_api(search_url, query, True, scrollId)
+    return response.json(), response.elapsed.total_seconds()
+
+
+def search_works(search_query):
+    response, elapsed = query_api("https://api.core.ac.uk/v3/search/works",
+                                  urllib.parse.quote(f"{search_query} and _exists_:description"))
+    search_results = []
+    if "results" not in response:
+        raise "Sorry, no results in CORE for your query."
+    print(len(response["results"]))
+    titles=[]
+    for hit in response["results"]:
+        search_results.append({"url": f"https://core.ac.uk/works/{hit['id']}", "abstract": f"{hit['abstract'][:800]}"})
+        titles.append(hit["title"])
+    if "results" not in response:
+        raise "Sorry, no results in CORE for your query."
+    return titles, search_results
